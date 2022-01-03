f2933dee6d9b760aa8c5a7639870d2e86f07b631
Mohamed Khaled <m.tawaty96@gmail.com>
2022-01-03 20:25:42 +0200

    Forward propagation updating

--- src/com/example/ann/ForwardPropagation.java
+++ src/com/example/ann/ForwardPropagation.java
@@ -5,24 +5,22 @@ public class ForwardPropagation {
     private double [][] outWeights;
 
 
-    //public void setHiddenWeights(double [][] hiddenWeights){this.hiddenWeights = hiddenWeights;}
-    //public void setOutWeights(double[][]outWeights){this.outWeights = outWeights;}
-
-    public double[] forwardPropa(double[]input, double[][] weights){
-        int l = weights.length;
-        int m = weights[0].length;
-//        System.out.println(m);
-//        System.out.println(l);
+    // forward Propagation method
+    public double[] forwardPropagation(double[]input, double[][] weights){
+        int l = weights.length;  //10
+        int m = weights[0].length; // 8
         double []hidden = new double[l];
         double result = 0.0;
 
-        for (int i=0 ; i<l ; i++){
-            for (int j=0 ; j<m ; j++){
+        for (int i=0 ; i<l ; i++){  // 2*  1
+            for (int j=0 ; j<m ; j++){  // 2*  10
                 result += input[j]*weights[i][j];
             }
             // sigmoid function
             result = 1/( 1 + Math.pow(Math.E,(-1*result)));
             hidden[i] = result;
+            //System.out.println(hidden[i]);
+            result = 0.0;
         }
 
         /**
