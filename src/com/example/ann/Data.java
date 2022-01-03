73db4818c9c46800eefb194e6cdb70229f5778d4
Mohamed Khaled <m.tawaty96@gmail.com>
2022-01-03 20:24:03 +0200

    Data updating

--- src/com/example/ann/Data.java
+++ src/com/example/ann/Data.java
@@ -16,9 +16,11 @@ public class Data {
     public static int N;
 
     public double[] getOutput(){return output;}
+    public double[] getInput(){return input;}
     public double[] getPredictedOutput(){return predictedOutput;}
-    public double[][] getOutWeights(){return outWeights;}
-                                                              // L        M
+    public double[] getHidden(){return hidden;}
+    //public double[][] getOutWeights(){return outWeights;}
+                                                              // L=10        M=8
     public double[][] WeightsInitialization(double[][]weights, int rew, int column){
         weights = new double[rew][column];
         double min = -2.0;
@@ -27,6 +29,7 @@ public class Data {
         //double value = min + (max - min) * random.nextDouble();
         for (int i=0 ; i<rew ; i++) {
             for (int j=0 ; j<column ; j++) {
+                // normalize the random double to be in the range
                 weights[i][j] = min + (max - min) * random.nextDouble();
             }
         }
@@ -52,64 +55,105 @@ public class Data {
             FileInputStream fis=new FileInputStream("train.txt");
             Scanner scannerReader=new Scanner(fis);    //file to be scanned
             String buffer;
+
             // Reading first rew data
             buffer = scannerReader.nextLine();
+            // split the record data
+            // fixed one column of hidden neurons in this Algorithm
             String[] splitValues = buffer.split(" ");
+            // Number of input neurons
             M = Integer.parseInt(splitValues[0]);
             input = new double[M];
+            // Number of hidden neurons
             L = Integer.parseInt(splitValues[1]);
+            // Number of output neurons
             N = Integer.parseInt(splitValues[2]);
             output = new double[N];
+            // avoiding the next line  "number of rew data"
             scannerReader.nextLine();
+            scannerReader.close();
 
-            BackPropagation backPropagation = new BackPropagation(M, L, N);
+            // opening the normalized file data
+            FileInputStream fileInputStream=new FileInputStream("normData.txt");
+            Scanner scannerRead=new Scanner(fileInputStream);    //file to be scanned
+
+
+            BackPropagation backPropagation = new BackPropagation();
+
+            // Weights Initialization Randomly
+            hiddenWeights = data.WeightsInitialization(hiddenWeights, L, M); //10*8
+            outWeights = data.WeightsInitialization(outWeights, N, L);
+
+            // Normalization
+            normalization.fileNormalization("train.txt", "normData.txt");
+
+            hidden = forward.forwardPropagation(input, hiddenWeights);
+            predictedOutput = forward.forwardPropagation(hidden, outWeights);
 
 
             int i, j;
-            while(scannerReader.hasNextLine())
+            double mse;
+            while(scannerRead.hasNextLine())
             {
 
                 //input data
                 for(i =0 ; i<M ; i++){
-                    input[i] = scannerReader.nextDouble();
+                    input[i] = scannerRead.nextDouble();
 
                 }
                 //output data
                 for(j =0 ; j<N ; j++){
-                    output[j] = scannerReader.nextDouble();
+                    output[j] = scannerRead.nextDouble();
+                }
+                scannerRead.nextLine();
+
+
+                for (i=0 ; i<500 ; i++) {
+
+                    //mse = backPropagation.MSE();
+                    if (backPropagation.MSE() > 0.9) {
+                        System.out.print("MSE ");
+                        System.out.println(backPropagation.MSE());
+
+                        // back propagation
+                        backPropagation.backPropagation(outWeights, hiddenWeights);
+
+                        // forward propagation
+                        hidden = forward.forwardPropagation(input, hiddenWeights);
+                        predictedOutput = forward.forwardPropagation(hidden, outWeights);
+
+                    }
+                    else {break;}
                 }
 
-                // Normalization
-                //input = normalization.inputNormalization(input);
-                // output = normalization.inputNormalization(output);
 
-                // Weights Initialization Randomly
-                hiddenWeights = data.WeightsInitialization(hiddenWeights, L, M);
-                outWeights = data.WeightsInitialization(outWeights, N, L);
-
-
-                // forward propagation
-                hidden = forward.forwardPropa(input, hiddenWeights);
-                predictedOutput = forward.forwardPropa(hidden, outWeights);
-/**********/
-                normalization.fileNormalization("train.txt", "normData.txt");
-
-                buffer = scannerReader.nextLine();      //returns the line that was skipped
+                //buffer = scannerRead.nextLine();      //returns the line that was skipped
                 break;
             }
 
             //System.out.print("MSE ");
             //System.out.println(backPropagation.MSE());
+/*
+            for (i=0 ; i<hiddenWeights.length; i++){
+                for (j=0 ; j<hiddenWeights[0].length ; j++){
+                    System.out.print(hiddenWeights[i][j]);
+                    System.out.print("   ");
+                }
+                System.out.println();
+            }
 
-            /*
+/*
             for (i=0 ; i< N ; i++)
                 System.out.println(output[i]);
+
+            for (i=0 ; i< M ; i++)
+                System.out.println(input[i]);
 
             /*
             for (String a : splitValues)
                 System.out.println(a);
              */
-            scannerReader.close();     //closes the scanner
+            scannerRead.close();     //closes the scanner
         }
         catch(IOException e)
         {
