db00062352cf0e5451f205c16185af6f9d940404
Mohamed Khaled <m.tawaty96@gmail.com>
2022-01-04 06:02:15 +0200

    Data class completed

--- src/com/example/ann/Data.java
+++ src/com/example/ann/Data.java
@@ -9,24 +9,20 @@ public class Data {
     public static double[] output;
     public static double[][] hiddenWeights;// Weights between input and hidden layer
     public static double[][] outWeights;   // Weights between hidden and output layer
-    public static double [] hidden;  // hidden layer output
-    public static double [] predictedOutput; // predicted Output layer values
     public static int M;
     public static int L;
     public static int N;
 
     public double[] getOutput(){return output;}
     public double[] getInput(){return input;}
-    public double[] getPredictedOutput(){return predictedOutput;}
-    public double[] getHidden(){return hidden;}
-    //public double[][] getOutWeights(){return outWeights;}
+    public double[][] getHiddenWeights(){return hiddenWeights;}
+    public double[][] getOutWeights(){return outWeights;}
                                                               // L=10        M=8
     public double[][] WeightsInitialization(double[][]weights, int rew, int column){
         weights = new double[rew][column];
         double min = -2.0;
         double max = 2.0;
         Random random = new Random();
-        //double value = min + (max - min) * random.nextDouble();
         for (int i=0 ; i<rew ; i++) {
             for (int j=0 ; j<column ; j++) {
                 // normalize the random double to be in the range
@@ -39,13 +35,8 @@ public class Data {
 
 
     public static void main(String[] args) {
-        //int M, L, N;
-        hiddenWeights = new double[L][M];
-        hidden = new double[M];
-        predictedOutput = new double[N];
-        //Normalization object to normalize the inputs
+
         Normalization normalization = new Normalization();
-        ForwardPropagation forward = new ForwardPropagation();
         Data data = new Data();
 
 
@@ -70,9 +61,22 @@ public class Data {
             N = Integer.parseInt(splitValues[2]);
             output = new double[N];
             // avoiding the next line  "number of rew data"
-            scannerReader.nextLine();
             scannerReader.close();
 
+            // Normalization file data
+            normalization.fileNormalization("train.txt", "normData.txt");
+
+            // Weights Initialization Randomly first time
+            hiddenWeights = data.WeightsInitialization(hiddenWeights, L, M); //10*8
+            outWeights = data.WeightsInitialization(outWeights, N, L);
+
+            Training training = new Training();
+            training.training("normData.txt");
+
+            Testing testing = new Testing();
+            testing.testing("test.txt");
+
+            /*
             // opening the normalized file data
             FileInputStream fileInputStream=new FileInputStream("normData.txt");
             Scanner scannerRead=new Scanner(fileInputStream);    //file to be scanned
@@ -80,15 +84,7 @@ public class Data {
 
             BackPropagation backPropagation = new BackPropagation();
 
-            // Weights Initialization Randomly
-            hiddenWeights = data.WeightsInitialization(hiddenWeights, L, M); //10*8
-            outWeights = data.WeightsInitialization(outWeights, N, L);
 
-            // Normalization
-            normalization.fileNormalization("train.txt", "normData.txt");
-
-            hidden = forward.forwardPropagation(input, hiddenWeights);
-            predictedOutput = forward.forwardPropagation(hidden, outWeights);
 
 
             int i, j;
@@ -107,13 +103,16 @@ public class Data {
                 }
                 scannerRead.nextLine();
 
+                hidden = forward.forwardPropagation(input, hiddenWeights);
+                predictedOutput = forward.forwardPropagation(hidden, outWeights);
+                //System.out.println(predictedOutput[0]);
 
                 for (i=0 ; i<500 ; i++) {
 
-                    //mse = backPropagation.MSE();
-                    if (backPropagation.MSE() > 0.9) {
-                        System.out.print("MSE ");
-                        System.out.println(backPropagation.MSE());
+                    mse = backPropagation.MSE();
+                    if (mse > 0.9) {
+                        //System.out.print("MSE ");
+                        //System.out.println(mse);
 
                         // back propagation
                         backPropagation.backPropagation(outWeights, hiddenWeights);
@@ -121,6 +120,7 @@ public class Data {
                         // forward propagation
                         hidden = forward.forwardPropagation(input, hiddenWeights);
                         predictedOutput = forward.forwardPropagation(hidden, outWeights);
+                        //System.out.println(predictedOutput[0]);
 
                     }
                     else {break;}
@@ -131,9 +131,7 @@ public class Data {
                 break;
             }
 
-            //System.out.print("MSE ");
-            //System.out.println(backPropagation.MSE());
-/*
+
             for (i=0 ; i<hiddenWeights.length; i++){
                 for (j=0 ; j<hiddenWeights[0].length ; j++){
                     System.out.print(hiddenWeights[i][j]);
@@ -142,18 +140,9 @@ public class Data {
                 System.out.println();
             }
 
-/*
-            for (i=0 ; i< N ; i++)
-                System.out.println(output[i]);
 
-            for (i=0 ; i< M ; i++)
-                System.out.println(input[i]);
-
-            /*
-            for (String a : splitValues)
-                System.out.println(a);
-             */
             scannerRead.close();     //closes the scanner
+            */
         }
         catch(IOException e)
         {
